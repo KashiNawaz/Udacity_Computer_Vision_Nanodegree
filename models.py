3d2f98b77c5b12e31b61d3750572e12dd2202b19
KashiNawaz <kashoo703@gmail.com>
2020-05-26 15:38:35 +0500

    updated the convolutional neural network architecture

--- models.py
+++ models.py
@@ -20,6 +20,43 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.drop1 = nn.Dropout(p=0.1)
+        # Output = (32, 220, 220)
+        # Maxpooled output = (32, 110, 110)
+        
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.drop2 = nn.Dropout(p=0.2)
+        # output = (64, 108, 108)
+        # Maxpooled output = (64, 54, 54)
+        
+        
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.pool3 = nn.MaxPool2d(2,2)
+        self.drop3 = nn.Dropout(p=0.3)
+        # Output = (128, 52, 52)
+        # Maxpooled Output = (128, 26, 26)
+        
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.pool4 = nn.MaxPool2d(2,2)
+        self.drop4 = nn.Dropout(p=0.4)
+        # Output = (256, 24, 24)
+        # Maxpooled Output = (256, 12, 12)
+        
+        self.conv5 = nn.Conv2d(256, 512, 3)
+        self.pool5 = nn.MaxPool2d(2,2)
+        self.drop5 = nn.Dropout(p=0.5)
+        # Output = (512, 10, 10)
+        # Maxpooled Output = (512, 5, 5)
+        
+        self.fc6 = nn.Linear(512*5*5, 2560)
+        self.drop6 = nn.Dropout(p=0.4)
+        
+        self.fc7 = nn.Linear(2560, 1280)
+        self.drop7 = nn.Dropout(p=0.4)
+
+        self.fc8 = nn.Linear(1280, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,7 +67,31 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.drop1(x)
         
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        
+        x = self.pool5(F.relu(self.conv5(x)))
+        x = self.drop5(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc6(x))
+        x = self.drop6(x)
+        
+        x = F.relu(self.fc7(x))
+        x = self.drop7(x)
+        
+        x = self.fc8(x)
+
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
